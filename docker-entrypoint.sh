167119d2cf020ad2cd5947c1bb20f9e1d0c0d69c
Gaël <liaudetgael@gmail.com>
2020-10-27 22:55:07 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+cd /etc/squid
+mkdir ssl_cert
+chown squid:squid ssl_cert
+cd ssl_cert
+openssl req -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -keyout proxyCA.pem  -out proxyCA.pem
+
+
 if [[ "$1" == "squid" ]]; then
     shift;
     /usr/sbin/squid ${@}
